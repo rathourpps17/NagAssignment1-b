2e367158eabff18f0be3c43673ac9583536257bc
rathourpushpendra17@gmail.com <rathourpushpendra17@gmail.com>
2022-06-27 19:44:23 +0530

    deleted

---
+++ liberary/src/main/java/com/nagarro/training/library/LiberaryApplication.java
@@ -0,0 +1,16 @@
+package com.nagarro.training.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiberaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiberaryApplication.class, args);
+		String ad="fgn";
+		System.out.println(ad);
+		
+	}
+
+}
---
+++ liberary/src/test/java/com/nagarro/liberary/LiberaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarro.liberary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiberaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
